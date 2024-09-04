dc7e5d3597b35f5556d341f02746d4ecb7bed908
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-09-03 23:55:22 -0300

    refactor: reset the entire project

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import { App } from './app.tsx';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import './index.css';
+
+const container = document.getElementById('root');
+
+createRoot(container!).render(<App />);
